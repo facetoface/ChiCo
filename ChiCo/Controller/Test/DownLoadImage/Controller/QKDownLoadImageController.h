f6b7e9f11cf6fb1ad9cba00f1a33b310a7a29f15
ChiCo <1392984568@qq.com>
2017-06-02 08:39:05 +0800

    学习图片下载

---
+++ ChiCo/Controller/Test/DownLoadImage/Controller/QKDownLoadImageController.h
@@ -0,0 +1,13 @@
+//
+//  QKDownLoadImageController.h
+//  ChiCo
+//
+//  Created by ChiCo on 2017/6/2.
+//  Copyright © 2017年 一米阳光. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QKDownLoadImageController : UIViewController
+
+@end
---
+++ ChiCo/Controller/Test/DownLoadImage/Controller/QKDownLoadImageController.m
@@ -0,0 +1,32 @@
+//
+//  QKDownLoadImageController.m
+//  ChiCo
+//
+//  Created by ChiCo on 2017/6/2.
+//  Copyright © 2017年 一米阳光. All rights reserved.
+//
+
+#import "QKDownLoadImageController.h"
+
+@interface QKDownLoadImageController ()
+
+@end
+
+@implementation QKDownLoadImageController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+- (void)dealloc
+{
+    NSLog(@"QKDownLoadImageController");
+}
+
+@end
