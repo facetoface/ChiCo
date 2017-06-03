bd7b11aab4ec12489ddc619d354d577973a453c4
言志 <1392984568@qq.com>
2017-06-03 10:46:53 +0800

    重新导入pod

--- Pods/Target Support Files/Pods-ChiCoTests/Pods-ChiCoTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ChiCoTests : NSObject
-@end
-@implementation PodsDummy_Pods_ChiCoTests
-@end
--- Pods/Target Support Files/Pods-ChiCoUITests/Pods-ChiCoUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ChiCoUITests : NSObject
-@end
-@implementation PodsDummy_Pods_ChiCoUITests
-@end
