b26ca50e25f8200434049de3c0ca0f0cbedc4424
言志 <1392984568@qq.com>
2017-06-03 10:37:55 +0800

    学习图片下载

---
+++ ChiCo/Controller/Test/LoadImage/Controller/QKWebImageDownloader.h
@@ -0,0 +1,13 @@
+//
+//  QKWebImageDownloader.h
+//  ChiCo
+//
+//  Created by 一米阳光 on 2017/6/3.
+//  Copyright © 2017年 一米阳光. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface QKWebImageDownloader : NSObject
+
+@end
---
+++ ChiCo/Controller/Test/LoadImage/Controller/QKWebImageDownloader.m
@@ -0,0 +1,13 @@
+//
+//  QKWebImageDownloader.m
+//  ChiCo
+//
+//  Created by 一米阳光 on 2017/6/3.
+//  Copyright © 2017年 一米阳光. All rights reserved.
+//
+
+#import "QKWebImageDownloader.h"
+
+@implementation QKWebImageDownloader
+
+@end
