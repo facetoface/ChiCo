d4383a21216a22e4be55003dfd830eee184f6f4b
ChiCo <1392984568@qq.com>
2017-06-19 16:26:20 +0800

    文件目录修改

---
+++ Diary/Resource/FrameConstants.h
@@ -0,0 +1,22 @@
+//
+//  FrameConstants.h
+//  ChiCo
+//
+//  Created by ChiCo on 2017/5/22.
+//  Copyright © 2017年 一米阳光. All rights reserved.
+//
+
+#ifndef FrameConstants_h
+#define FrameConstants_h
+
+// 屏幕宽度
+#define SCREEN_WIDTH ([[UIScreen mainScreen]bounds].size.width)
+
+//屏幕高度
+#define SCREEN_HEIGHT ([[UIScreen mainScreen]bounds].size.height)
+
+//1像素
+#define ONEPIX (CGFloat)(1.0 / [UIScreen mainScreen].scale)
+#define QK_PADDING 10
+
+#endif /* FrameConstants_h */
