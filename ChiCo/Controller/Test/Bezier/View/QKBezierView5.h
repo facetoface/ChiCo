b3134848aa2a0179245d1bf3e96ddf0865e7198b
ChiCo <1392984568@qq.com>
2017-06-26 11:48:12 +0800

    添加粒子束

---
+++ ChiCo/Controller/Test/Bezier/View/QKBezierView5.h
@@ -0,0 +1,13 @@
+//
+//  QKBezierView5.h
+//  ChiCo
+//
+//  Created by ChiCo on 2017/6/26.
+//  Copyright © 2017年 一米阳光. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QKBezierView5 : UIView
+
+@end
---
+++ ChiCo/Controller/Test/Bezier/View/QKBezierView5.m
@@ -0,0 +1,54 @@
+//
+//  QKBezierView5.m
+//  ChiCo
+//
+//  Created by ChiCo on 2017/6/26.
+//  Copyright © 2017年 一米阳光. All rights reserved.
+//
+
+#import "QKBezierView5.h"
+
+@implementation QKBezierView5
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        
+        [self setBackgroundColor:[UIColor lightGrayColor]];
+        
+        CALayer *layer1 = [CALayer layer];
+        [layer1 setFrame:CGRectMake(10, 10, 50, 50)];
+        layer1.cornerRadius = 5;
+        [layer1 setBackgroundColor:[UIColor cyanColor].CGColor];
+        [self.layer addSublayer:layer1];
+        
+        CAEmitterLayer *emitterLayer = [CAEmitterLayer layer];
+        [emitterLayer setFrame:CGRectMake(70, 10, 150, 150)];
+        [emitterLayer setBackgroundColor:[UIColor lightTextColor].CGColor];
+        [self.layer addSublayer:emitterLayer];
+        emitterLayer.emitterShape = kCAEmitterLayerPoint;
+       
+        CAEmitterCell *cell = [CAEmitterCell emitterCell];
+        [cell setBirthRate:3];
+        [cell setContentsRect:CGRectMake(0, 0, 20, 20)];
+        cell.emissionLatitude = M_PI_2+M_PI;
+        cell.emissionRange = M_PI_2;
+
+        cell.contents = CFBridgingRelease([UIImage imageNamed:@"fish_highlight"].CGImage);
+        cell.lifetime = 4;
+        cell.velocity = 100;
+        cell.velocityRange = 80;
+        cell.scale = 0.2;
+        cell.velocityRange = M_PI_2;
+       
+    
+        
+        emitterLayer.emitterCells = @[cell];
+        
+        
+    }
+    return self;
+}
+
+@end
