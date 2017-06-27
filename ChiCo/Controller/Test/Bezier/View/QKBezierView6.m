a933ad2ebcb489f10c027116b340c4cdeed12c37
ChiCo <1392984568@qq.com>
2017-06-27 15:23:22 +0800

    添加画圆

--- ChiCo/Controller/Test/Bezier/View/QKBezierView6.m
+++ ChiCo/Controller/Test/Bezier/View/QKBezierView6.m
@@ -36,7 +36,7 @@
     CGContextAddLineToPoint(ctx, 110, 100);
     CGContextAddLineToPoint(ctx, 0, 100);
     CGContextAddLineToPoint(ctx, 10, 10);
-   
+    
     /************************************
      画边框
      CGContextStrokePath(ctx);
@@ -46,9 +46,40 @@
      但是 CGContextStrokePath 和 CGContextFillPath 不可以同时使用
      如果同时使用,写在前边的会生效
      ************************************/
-     CGContextDrawPath(ctx, kCGPathEOFillStroke);
+    CGContextDrawPath(ctx, kCGPathEOFillStroke);
 
+//    CGContextAddRect(ctx, CGRectMake(120, 120, 60, 60));
+    CGContextMoveToPoint(ctx, 180, 280-70);  // 开始坐标右边开始
+    CGContextAddArcToPoint(ctx, 180, 280, 180-20, 280, 10);  // 右下角角度
+    CGContextAddArcToPoint(ctx, 120, 280, 120, 280-10, 10); // 左下角角度
+    CGContextAddArcToPoint(ctx, 120, 200, 180-20, 200, 10); // 左上角
+    CGContextAddArcToPoint(ctx, 180, 200, 180, 280-70, 10); // 右上角
+    CGContextStrokePath(ctx);
     
+//    CGFloat arcLenth = 10;
+//    CGFloat arcRadio = 60;
+//    //从左上角开始
+//    CGContextMoveToPoint(ctx, 120+arcRadio, 120);
+//    //画右上角的拐角
+//    CGContextAddArcToPoint(ctx, 180, 120, 180, 120+arcLenth, arcRadio);
+//    CGContextAddArcToPoint(ctx, 180, 180, 180-arcLenth, 180, arcRadio);
+//    CGContextAddArcToPoint(ctx, 120, 180, 120, 180-arcLenth, arcRadio);
+//    CGContextAddArcToPoint(ctx, 120, 120, 180, 120, arcRadio);
+//    CGContextFillPath(ctx);
+//    CGContextClosePath(ctx);
+    
+
+    CGContextAddArc(ctx, 150, 40, 30, 0, 2*M_PI, 0);
+    CGContextFillPath(ctx);
+    CGContextAddArc(ctx, 210, 40, 30, 0, 2*M_PI, 0);
+    CGContextFillPath(ctx);
+    CGContextAddArc(ctx, 210, 100, 30, 0, 2*M_PI, 0);
+    CGContextFillPath(ctx);
+    CGContextAddArc(ctx, 150, 100, 30, 0, 2*M_PI, 0);
+    CGContextAddRect(ctx, CGRectMake(150, 40, 60, 60));
+    CGContextFillPath(ctx);
+  
+
 }
 
 
