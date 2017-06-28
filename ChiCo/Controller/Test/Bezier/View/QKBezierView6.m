a3f3303110fc586b5e72008305a156735288c989
ChiCo <1392984568@qq.com>
2017-06-28 10:40:49 +0800

    画圆角研究

--- ChiCo/Controller/Test/Bezier/View/QKBezierView6.m
+++ ChiCo/Controller/Test/Bezier/View/QKBezierView6.m
@@ -43,7 +43,7 @@
     CGContextAddLineToPoint(ctx, 110, 100);
     CGContextAddLineToPoint(ctx, 0, 100);
     CGContextAddLineToPoint(ctx, 10, 10);
-    
+    CGContextDrawPath(ctx, kCGPathEOFillStroke);
     /************************************
      画边框
      CGContextStrokePath(ctx);
@@ -53,18 +53,8 @@
      但是 CGContextStrokePath 和 CGContextFillPath 不可以同时使用
      如果同时使用,写在前边的会生效
      ************************************/
-    CGContextDrawPath(ctx, kCGPathEOFillStroke);
-
-//    CGContextAddRect(ctx, CGRectMake(120, 120, 60, 60));
-    CGContextMoveToPoint(ctx, 180, 280-70);  // 开始坐标右边开始
-    CGContextAddArcToPoint(ctx, 180, 280, 180-20, 280, 10);  // 右下角角度
-    CGContextAddArcToPoint(ctx, 120, 280, 120, 280-10, 10); // 左下角角度
-    CGContextAddArcToPoint(ctx, 120, 200, 180-20, 200, 10); // 左上角
-    CGContextAddArcToPoint(ctx, 180, 200, 180, 280-70, 10); // 右上角
-    CGContextStrokePath(ctx);
-    
     CGFloat arcLenth = 10;
-    CGFloat arcRadio = 10;
+    CGFloat arcRadio = 15;
     CGPoint startPoint = CGPointMake(250, 10);
     
     //从左上角开始
@@ -80,6 +70,7 @@
     CGContextClosePath(ctx);
     
 
+    //画个4叶草
     CGContextAddArc(ctx, 150, 40, 30, 0, 2*M_PI, 0);
     CGContextFillPath(ctx);
     CGContextAddArc(ctx, 210, 40, 30, 0, 2*M_PI, 0);
@@ -89,8 +80,43 @@
     CGContextAddArc(ctx, 150, 100, 30, 0, 2*M_PI, 0);
     CGContextAddRect(ctx, CGRectMake(150, 40, 60, 60));
     CGContextFillPath(ctx);
-  
-
+    
+    //CGContextAddArcToPoint 研究
+    CGContextAddRect(ctx, CGRectMake(10, 150, 80, 80));
+    CGContextStrokePath(ctx);
+    CGContextAddArc(ctx, 50, 190, 40, 0, 2*M_PI, 40);
+    CGContextStrokePath(ctx);
+    //画1个圆角
+    CGContextAddRect(ctx, CGRectMake(100, 150, 80, 80));
+    CGContextMoveToPoint(ctx, 100, 150);
+    CGContextAddArcToPoint(ctx, 180, 150, 180, 160, 40);
+    CGContextStrokePath(ctx);
+    //画2个圆角
+    CGContextAddRect(ctx, CGRectMake(190, 150, 80, 80));
+    CGContextMoveToPoint(ctx, 190, 150);
+    CGContextAddArcToPoint(ctx, 270, 150, 270, 160, 40);
+    CGContextAddArcToPoint(ctx, 270, 230, 260, 230, 40);
+    CGContextStrokePath(ctx);
+    //画3个圆角
+    CGContextAddRect(ctx, CGRectMake(280, 150, 80, 80));
+    CGContextMoveToPoint(ctx, 280, 150);
+    CGContextAddArcToPoint(ctx, 360, 150, 360, 160, 40);
+    CGContextAddArcToPoint(ctx, 360, 230, 350, 230, 40);
+    CGContextAddArcToPoint(ctx, 280, 230, 280, 210, 40);
+    CGContextStrokePath(ctx);
+    
+    //画4个圆角
+    CGContextAddRect(ctx, CGRectMake(10, 240, 80, 80));
+    CGContextMoveToPoint(ctx, 10+40, 240);
+    CGContextAddArcToPoint(ctx, 90, 240, 90, 250, 40);
+    CGContextAddArcToPoint(ctx, 90, 320, 80, 320, 40);
+    CGContextAddArcToPoint(ctx, 10, 320, 10, 310, 40);
+    CGContextAddArcToPoint(ctx, 10, 240, 20, 240, 40);
+    CGContextStrokePath(ctx);
+    
+    /**************************************************
+     CGContextAddArcToPoint
+    **************************************************/
 }
 
 
@@ -112,11 +138,6 @@
         [self.layer addSublayer:layer];
     }
     return self;
-}
-
-- (void)drawRect:(CGRect)rect
-{
-  
 }
 
 
