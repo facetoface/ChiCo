83b84b0e096c82681fd35744122b45ede7f8cb35
ChiCo <1392984568@qq.com>
2017-07-06 10:25:54 +0800

    添加动画测试

---
+++ ChiCo/Controller/Test/Animation/AnimationController.h
@@ -0,0 +1,13 @@
+//
+//  AnimationController.h
+//  ChiCo
+//
+//  Created by ChiCo on 2017/7/6.
+//  Copyright © 2017年 一米阳光. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AnimationController : UIViewController
+
+@end
