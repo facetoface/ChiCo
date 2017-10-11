2cc969688f8196f3b4ee57e7af40653b884b318b
ChiCo <1392984568@qq.com>
2017-10-11 15:36:07 +0800

    开始整理运行时

---
+++ ChiCo/Controller/Test/Runtime/RunTimeController.h
@@ -0,0 +1,13 @@
+//
+//  RunTimeController.h
+//  ChiCo
+//
+//  Created by ChiCo on 2017/10/11.
+//  Copyright © 2017年 一米阳光. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RunTimeController : UIViewController
+
+@end
---
+++ ChiCo/Controller/Test/Runtime/RunTimeController.m
@@ -0,0 +1,29 @@
+//
+//  RunTimeController.m
+//  ChiCo
+//
+//  Created by ChiCo on 2017/10/11.
+//  Copyright © 2017年 一米阳光. All rights reserved.
+//
+
+#import "RunTimeController.h"
+
+@interface RunTimeController ()
+
+@end
+
+@implementation RunTimeController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+@end
